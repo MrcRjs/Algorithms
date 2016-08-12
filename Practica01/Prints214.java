2ec2c66e0d11e2a81e01257b981bb329a5ff7b28
Marco A. Rojas <contact@marcorojas.me>
2016-08-12 13:03:58 -0500

    2.14: Imprime utilizando println, print y printf

---
+++ Practica01/Prints214.java
@@ -0,0 +1,13 @@
+public class Prints214 {
+  public static void main(String[] args) {
+    // Mediante una instrucción println()
+    System.out.println("1 2 3 4");
+    // Mediante 4 instrucciones print()
+    System.out.print("1 ");
+    System.out.print("2 ");
+    System.out.print("3 ");
+    System.out.print("4");
+    // Mediante una instrucción printf
+    System.out.printf("\n1 2 3 4");
+  }
+}
